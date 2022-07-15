fc4a57f5810a36b91f427377136c56fabe010116
UltraMadd <Victor.main1386@gmail.com>
2022-07-15 19:13:09 +0700

    DetectionInfo, descriptions

--- src/circuit/detections.py
+++ src/circuit/detections.py
@@ -1,2 +1,28 @@
 class DetectionTypes:
-    descriptions = {}
+    descriptions = {
+        "0001": "Too many files in folder",
+        "0002": "Too many folders",
+        "0003": "Too long folder name",
+        "0004": "Too long file name",
+        "0005": "Empty folder"
+    }
+
+
+class DetectionInfo:
+    info = {
+        "0001": (
+            "Dir", "M"
+        ),
+        "0002": (
+            "Dir", "W"
+        ),
+        "0003": (
+            "Dir", "W"
+        ),
+        "0004": (
+            "Dir", "M"
+        ),
+        "0005": (
+            "Dir", "M"
+        )
+    }
